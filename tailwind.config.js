1fce6ab947131150fe567b952d4c9026e0cff09a
Aleandro Coppola <a.coppola@sintraconsulting.eu>
2021-11-30 22:14:41 +0100

    disable purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   important: true,
   purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    // './components/**/*.{vue,js}',
+    // './layouts/**/*.vue',
+    // './pages/**/*.vue',
+    // './plugins/**/*.{js,ts}',
+    // './nuxt.config.{js,ts}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
