e93f5c1c446149a517996003c801495df82c0bce
Aleandro Coppola <a.coppola@sintraconsulting.eu>
2021-11-30 18:43:34 +0100

    improvements

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container m-auto">
     <div class="w-full flex justify-between items-center border-b-2 pb-4">
       <span>Mosse: {{ totalClicks }}</span>
+
       <div class="flex items-center">
         Record: {{ record }} Mosse
         <button
@@ -18,16 +19,17 @@
         v-for="(card, index) in grid"
         :key="index"
         class="h-24 w-full bg-gray-100 cursor-pointer"
-        v-bind:class="{
-          ['bg-' + card + '-500']:
-            visibleCards.includes(index) || selectedCards.includes(index),
-        }"
+        v-bind:class="
+          cardsFounds.includes(index) || faceUpCards.includes(index)
+            ? `bg-${card}-500 pointer-events-none`
+            : ''
+        "
         v-on:click="selectCard(index)"
       ></div>
     </div>
 
     <div
-      v-if="visibleCards.length === grid.length && grid.length !== 0"
+      v-if="cardsFounds.length === grid.length && grid.length !== 0"
       class="
         mt-8
         text-center
@@ -58,8 +60,8 @@ export default {
     return {
       cards: ['blue', 'green', 'red', 'yellow', 'purple', 'pink'],
       grid: [],
-      visibleCards: [],
-      selectedCards: [],
+      cardsFounds: [],
+      faceUpCards: [],
       timer: null,
       totalClicks: 0,
       record: 0,
@@ -75,38 +77,40 @@ export default {
     },
     selectCard(index) {
       if (
-        this.visibleCards.includes(index) ||
-        this.selectedCards.includes(index)
+        this.cardsFounds.includes(index) ||
+        this.faceUpCards.includes(index)
       ) {
         return;
       }
 
-      if (this.selectedCards.length == 2) {
+      // Clear on click, before timeout
+      if (this.faceUpCards.length == 2) {
         clearTimeout(this.timer);
-        this.selectedCards = [];
+        this.faceUpCards = [];
+        return;
       }
 
-      this.selectedCards.push(index);
+      this.faceUpCards.push(index);
       this.totalClicks++;
 
-      if (this.selectedCards.length == 2) {
-        if (
-          this.grid[this.selectedCards[0]] === this.grid[this.selectedCards[1]]
-        ) {
-          this.visibleCards.push(...this.selectedCards);
+      if (this.faceUpCards.length === 2) {
+        if (this.grid[this.faceUpCards[0]] === this.grid[this.faceUpCards[1]]) {
+          this.cardsFounds.push(...this.faceUpCards);
+          this.faceUpCards = [];
+        } else {
+          this.timer = setTimeout(() => {
+            this.faceUpCards = [];
+          }, 2000);
         }
-
-        clearTimeout(this.timer);
-        this.timer = setTimeout(() => {
-          this.selectedCards = [];
-        }, 5000);
       }
 
-      if (this.visibleCards.length === this.grid.length) {
-        this.record =
-          this.totalClicks > this.record && this.record !== 0
-            ? this.record
-            : this.totalClicks;
+      this.checkGrid();
+    },
+    checkGrid() {
+      if (this.cardsFounds.length === this.grid.length) {
+        if (this.totalClicks < this.record || this.record === 0) {
+          this.record = this.totalClicks;
+        }
         setTimeout(() => {
           this.startGame();
         }, 2000);
@@ -116,8 +120,8 @@ export default {
       clearTimeout(this.timer);
       this.totalClicks = 0;
       this.grid = [];
-      this.visibleCards = [];
-      this.selectedCards = [];
+      this.cardsFounds = [];
+      this.faceUpCards = [];
       this.generateGrid();
     },
   },
